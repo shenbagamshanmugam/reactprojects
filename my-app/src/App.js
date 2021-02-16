44c3d417451921aef5d65ea09dc77f0f7f4629e6
Shenbagam S <shenbagam.s@intimetec.com>
2021-02-16 12:46:13 +0530

    initial

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="person">
+        <h1>Name:Shenba</h1>
+        <p>Age:24</p>
+      </div>
+      <div className="person">
+        <h1>Name:Sudar</h1>
+        <p>Age:21</p>
+      </div>
+    </div>    
+  );
+}
+
+export default App;
